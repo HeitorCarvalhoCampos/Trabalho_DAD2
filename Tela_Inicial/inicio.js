754bd8d38c7b99a909aa10a054642090edeea0f4
HeitorCarvalhoCampos <heitor.campos@germinare.org.br>
2024-06-11 12:59:27 -0300

    Tela de inicio pronta

---
+++ Tela_Inicial/inicio.js
@@ -0,0 +1,19 @@
+// const botao_iniciar = document.querySelectorAll(".botao_iniciar");
+// var click = document.getElementById("click");
+
+// botao_iniciar.addEventListener("click", () => {
+//     click.play()
+// });
+
+// function som() {
+//     const audio = new Audio();
+//     audio.src = "../assets/click.mp3";
+//     audio.onerror = () => {
+//         console.error("Erro ao carregar o áudio");
+//     };
+    
+//     audio.onplay = () => {
+//         console.log("Áudio começou a ser reproduzido");
+//     };
+//     audio.play();
+// }
